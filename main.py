87e276cf8c41b48d8ad0f11729fefcbe0d5b1408
Amit Keren <amitkeren9@gmail.com>
2020-10-04 17:01:17 +0300

    strategies and reformatting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from envelope import Envelope
-from strategy import BaseStrategy, Automatic_BaseStrategy,N_max_strategy, More_then_N_percent_group_strategy
-def cls(): print ("\n" * 20)
+from strategy import BaseStrategy, Automatic_BaseStrategy, N_max_strategy, More_then_N_percent_group_strategy
+
+
+def cls(): print("\n" * 20)
+
+
 envelopes = []
 for i in range(100):
     envelopes.append(Envelope())
@@ -11,23 +15,24 @@ strategies.append(Automatic_BaseStrategy(envelopes))
 # random selection of envelop
 strategies.append(N_max_strategy(envelopes))
 # return envelope after N max values (defualt N=3)
-strategies.append(More_then_N_percent_group_strategy(envelopes, 0.25)) # return envelope with more money that in thehighest of N% group
-n=-1
-while n!=4:
+strategies.append(More_then_N_percent_group_strategy(envelopes,
+                                                     0.25))  # return envelope with more money that in thehighest of N% group
+n = -1
+while n != 4:
     cls()
     for i in range(len(strategies)):
         print(i, strategies[i].display())
     n = input(f'enter your choice [0-{len(strategies)}](len(strategies) to end):')
     if n.isdigit():
-        n=int(n)
-        if n==2:
+        n = int(n)
+        if n == 2:
             N = input(f'enter N max values: ')
-            strategies[n].N=int(N)
-        elif n==3:
+            strategies[n].N = int(N)
+        elif n == 3:
             p = input(f'enter 0-1 number for group size(defualt=0.25)')
-            strategies[n].percent=p
-        if n!=4:
+            strategies[n].percent = p
+        if n != 4:
             strategies[n].play()
-        x=input('press any key to continue')
+        x = input('press any key to continue')
     else:
         pass
