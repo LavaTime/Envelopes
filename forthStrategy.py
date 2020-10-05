f5dc53a371e4b0185fca7d910e77ad11f951de62
Amit Keren <amitkeren9@gmail.com>
2020-10-05 16:19:21 +0300

    docstring and specifics in forthStrategy.py

--- forthStrategy.py
+++ forthStrategy.py
@@ -1,9 +1,28 @@
 from envelope import Envelope
 
+
 class N_max_strategy:
+    """
+    can run the forth strategy on a set of envelops
+    Attributes:
+        envelops (list): list of envelops to run the strategy one
+        N (int): holds the N for the strategy
+    """
+
     def __init__(self, envelops):
         self.envelops = envelops
-        self.N = None
+        self._N = None
+
+    @property
+    def N(self):
+        return self._N
+
+    @N.setter
+    def N(self, newN):
+        if newN is int:
+            self._N = newN
+        else:
+            print('N can only be an integer')
 
     def play(self):
         """
@@ -24,6 +43,16 @@ class N_max_strategy:
         self.perform_strategy(self.N)
 
     def perform_strategy(self, counter):
+        """
+        perform the forth strategy, that opens envelopes that are bigger with some count
+
+        Args:
+            counter (int): how many bigger envelops to open before stopping
+
+        Returns:
+            None
+
+        """
         n_biggest = [self.envelops[0].money]
         i = 0
         while len(n_biggest) < counter:
@@ -34,6 +63,7 @@ class N_max_strategy:
                 n_biggest.append(self.envelops[i].money)
             i += 1
         print(str(n_biggest))
+
 
 envs = []
 for i in range(10000000):
